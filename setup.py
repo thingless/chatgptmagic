257b5fb3dae8bf7737423607057bbd530348c3d7
freethenation <ftn@dyno>
2023-03-03 12:20:12 -0500

    cleaup

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-import os
 from setuptools import setup
-
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-
-setup(name='chatgptmagic',
-      version='0.3',
-      description=' ipython magic for chatgpt ',
-      url='https://github.com/thingless/chatgptmagic',
-      install_requires=required,
-     )
+setup(
+    name='chatgptmagic',
+    version='0.3',
+    description=' ipython magic for chatgpt ',
+    url='https://github.com/thingless/chatgptmagic',
+    install_requires=["rich","openai"],
+)
