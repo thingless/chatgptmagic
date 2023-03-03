304473139fa7d2fa4ed99bd03aaf3c63dac5083c
freethenation <ftn@dyno>
2023-03-03 12:24:08 -0500

    chekc if OPENAI_API_KEY var set

--- chatgptmagic/__init__.py
+++ chatgptmagic/__init__.py
@@ -1,6 +1,10 @@
 __version__ = '0.0.1'
 
+import os
+import sys
 from .chatgpt_magic import ChatGptMagic
 
 def load_ipython_extension(ipython):
+    if not os.getenv('OPENAI_API_KEY'):
+        print('you must specify "OPENAI_API_KEY" enviroment variable to make ai magic work', file=sys.stderr)
     ipython.register_magics(ChatGptMagic)
