5d2b6dd91b2adf7b9678d2a475699c754bd8b2ed
freethenation <ftn@dyno>
2023-03-03 12:09:56 -0500

    better setup

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ from setuptools import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(install_requires=required)
+
+setup(name='chatgptmagic',
+      version='0.3',
+      description=' ipython magic for chatgpt ',
+      url='https://github.com/thingless/chatgptmagic',
+      install_requires=required,
+     )
